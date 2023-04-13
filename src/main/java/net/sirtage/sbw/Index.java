19823cb5a3323a6f3b87fe113b9a4abcb91c62ed
Sirtage <zirocilt@gmail.com>
2023-04-13 20:37:48 +0300

    This is a new commit for what I originally planned to be amended

---
+++ src/main/java/net/sirtage/sbw/Index.java
@@ -0,0 +1,19 @@
+package net.sirtage.sbw;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = Index.MODID, version = Index.VERSION)
+public class Index
+{
+    public static final String MODID = "sbwmod";
+    public static final String VERSION = "1.0";
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+		
+    }
+}
---
+++ src/main/java/net/sirtage/sbw/vp/MColor.java
@@ -0,0 +1,15 @@
+package net.sirtage.sbw.vp;
+
+public class MColor {
+    public final int r;
+    public final int g;
+    public final int b;
+    public final float a;
+
+    public MColor(int r, int g, int b, float a) {
+        this.r = r;
+        this.g = g;
+        this.b = b;
+        this.a = a;
+    }
+}
